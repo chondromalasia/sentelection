a412308c99a06c5125043332150b1c970bc2ff53
Heath Gordon <angrydogproductions@gmail.com>
2017-03-31 17:54:26 +0200

    extracted ngrams

--- sentelection.py
+++ sentelection.py
@@ -6,6 +6,8 @@ import os
 import codecs
 import csv
 import nltk.stem
+import nltk.corpus
+import nltk
 import sys
 # allows csv to do utf-8 encoding
 reload(sys)
@@ -81,23 +83,68 @@ def normalizer(unnormalized_list):
 				unnormalized_list[i][5][j][2] = lemmatizer.lemmatize(thing[1], 'r')
 			elif thing[3] == 'A':
 				unnormalized_list[i][5][j][2] = lemmatizer.lemmatize(thing[1], 'a')
-		
-
-	"""
-	for i,thing in enumerate(unnormalized_list[1][5]):
-		print thing
-		if thing[3] == 'V':
-			print lemmatizer.lemmatize(thing[1], 'v')
-		elif thing[3] == 'N':
-			print lemmatizer.lemmatize(thing[1])
-		elif thing[3] == 'R':
-			print lemmatizer.lemmatize(thing[1], 'r')
-		elif thing[3] == 'A':
-			print lemmatizer.lemmatize(thing[1], 'a')
-	"""
-
 	return unnormalized_list
 
+def dict_compare(token, dict_to_increment):
+	if token in dict_to_increment:
+		dict_to_increment[token] += 1
+	else:
+		dict_to_increment[token] = 1
+	return dict_to_increment
+
+def sort_dict(dict_to_sort):
+	return [(k, dict_to_sort[k]) for k in sorted(dict_to_sort, key=dict_to_sort.get, reverse=True)]
+
+def ngram_extractor(to_extract):
+	""" This function gets the top n unigrams, bigrams and trigrams
+	right now they're lowercased
+	"""
+
+	n = 100
+
+	unigram_dict = {}
+	bigram_dict = {}
+	trigram_dict = {}
+
+	stopwords = nltk.corpus.stopwords.words('english')
+	stopwords = stopwords + [',', '.', '!', '"', '?', ':', "'", '&']
+
+	for i, tweet in enumerate(to_extract):
+		# check to see if there's a normalized form
+		for i,token_list in enumerate(tweet[5]):
+			if token_list[2] != '_' and token_list[2] not in stopwords:
+				unigram_dict = dict_compare(token_list[2].lower(), unigram_dict)
+			elif token_list[1] not in stopwords:
+				unigram_dict = dict_compare(token_list[1].lower(), unigram_dict)
+
+	for i, tweet in enumerate(to_extract):
+		tokens = []
+		for token_list in tweet[5]:
+			if token_list[2] != "_":
+				tokens.append(token_list[2].lower())
+			else:
+				tokens.append(token_list[1].lower())
+
+		# fill out bigrams dictionary
+		for i in nltk.ngrams(tokens,2):
+			bigram_dict = dict_compare(i, bigram_dict)
+
+		# I'm using _EOT_ to signify the tweet boundaries
+		bigram_dict = dict_compare(("_EOT_", tokens[0]), bigram_dict)
+		bigram_dict = dict_compare((tokens[len(tokens)-1], "_EOT_"), bigram_dict)
+
+		# and trigrams
+		for i in nltk.ngrams(tokens, 3):
+			trigram_dict = dict_compare(i, trigram_dict)
+
+		trigram_dict = dict_compare(("_EOT_", tokens[0], tokens[1]), trigram_dict)
+		trigram_dict = dict_compare((tokens[len(tokens)-2], tokens[len(tokens)-1], "_EOT_"), trigram_dict)
+
+	sorted_unigrams = sort_dict(unigram_dict)
+	sorted_bigrams = sort_dict(bigram_dict)
+	sorted_trigrams = sort_dict(trigram_dict)
+
+	return (sorted_unigrams[:n], sorted_bigrams[:n], sorted_trigrams[:n])
 
 def main():
 	# read the untagged tweets
@@ -115,10 +162,11 @@ def main():
 	joined_train = join_tweets(tagged_train_tweets, train_tweets)
 	joined_test = join_tweets(tagged_test_tweets, test_tweets)
 
+	# lemmatizes the tokens
 	normalized_train = normalizer(joined_train)
-	for i in normalized_train[1][5]:
-		print i
 
+	feature_ngrams = ngram_extractor(normalized_train)
+	print feature_ngrams
 
 if __name__ == "__main__":
 	main()
